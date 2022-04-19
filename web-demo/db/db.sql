02b520f6dd633ffc3adbf45802e4a41806d39c34
mandy900619 <73085388+mandy900619@users.noreply.github.com>
2022-04-19 17:31:46 +0800

    Delete web-demo directory

--- web-demo/db/db.sql
+++
@@ -1,9 +0,0 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+08:00";
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL auto_increment,
-  `username` varchar(64) NOT NULL,
-  `password` varchar(64) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
