66f9f91d9e0dd2836c3684dda6c55d8a473191b2
mandy900619 <73085388+mandy900619@users.noreply.github.com>
2022-04-25 23:39:57 +0800

    Delete www directory

--- www/admin.php
+++
@@ -1,6 +0,0 @@
-<form action="admin.inc.php" method="post" >
-    <input id="title" type="text" name="title" placeholder="輸入標題...">
-    <br>
-    <button type="submit" name="submit">送出</button> 
-</form>
-<a href='index.php'>Home page</a>
